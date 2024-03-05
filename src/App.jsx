70626b68bf64bce81e782df086ec58ff751a0e25
farhan <el3317.farhanmahmood@gmail.com>
2024-03-05 12:46:09 +0530

    fetch api example

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import "./App.css";
 // import Example from "./useEffect/Example";
 // import Counter from "./useState/Counter";
-import MapExample from "./MapFuncExample/MapExample";
+// import MapExample from "./MapFuncExample/MapExample";
+import FetchApi from "./ApiExample/FetchApi";
 
 const App = () => {
   return (
     <>
-      {/* <Counter/> */}
+      {/* <Counter/>
       {/* <Example/> */}
-      <MapExample/>
+      {/* <MapExample/> */} 
+      <FetchApi/>
     </>
   );
 };
