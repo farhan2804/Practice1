e919ad9b6da9601cbf65e78561765d93412bfbf9
farhan <el3317.farhanmahmood@gmail.com>
2024-03-25 22:21:38 +0530

    updated the map function example

--- src/MapFuncExample/MapExample.jsx
+++ src/MapFuncExample/MapExample.jsx
@@ -1,17 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Data } from "../Mapdata";
-
 const MapExample = () => {
   return (
     <>
       {Data.map((response) => (
         <div key={response.id}>
           <h1>{response.name}</h1>
-          <li>{response.title}</li>
+          <h2>{response.title}</h2>
         </div>
       ))}
     </>
   );
 };
-
 export default MapExample;
